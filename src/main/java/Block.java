2667716581c16db265bf22898356de11d86eae66
Darlan Noetzold <darlan.noetzold@gmail.com>
2022-01-08 08:02:30 -0300

    Update Block.java

--- src/main/java/Block.java
+++ src/main/java/Block.java
@@ -7,9 +7,20 @@ public class Block {
     private String data;
     private long timeStamp;
 
-    public Block(String data,String previousHash ) {
+    public Block(String data, String previousHash) {
         this.data = data;
         this.previousHash = previousHash;
         this.timeStamp = new Date().getTime();
+        this.hash = calculateHash(); //Making sure we do this after we set the other values.
+    }
+
+    public String calculateHash() {
+        String calculatedhash = StringUtil.applySha256(
+                previousHash +
+                        Long.toString(timeStamp) +
+                        data
+        );
+        return calculatedhash;
+
     }
 }
